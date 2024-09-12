25a3de814e156c9b41b3d93e0fe0415c71b3dcef
devAdmin <devadmin@root.local>
2024-09-12 22:14:04 +0545

    vertical gap needed

--- calculator.cpp
+++ calculator.cpp
@@ -15,7 +15,7 @@ int main(){
         }
 
         //background color
-        window.clear(sf::Color::White);
+        window.clear(sf::Color(30, 30, 30));
 
         //font
         sf::Font font;
@@ -24,55 +24,36 @@ int main(){
             return 1;
         }
 
-        //header text and its position
-        sf::Text calc("Calculator", font);
-        calc.setCharacterSize(20);
-        calc.setFillColor(sf::Color::Red);
-        calc.setPosition(sf::Vector2f(150, 1));
-        window.draw(calc);
-
         //calculator display screen
-        sf::RectangleShape display(sf::Vector2f(300,50));
-        display.setPosition(sf::Vector2f(50, calc.getGlobalBounds().height + 10));
+        sf::RectangleShape display(sf::Vector2f(334,107));
+        display.setPosition(sf::Vector2f(33, 51));
         display.setFillColor(sf::Color::White);
         display.setOutlineColor(sf::Color::Black);
         display.setOutlineThickness(2);
         window.draw(display);
 
-        //numbers
-        sf::Text number[10];
-        for(int i = 0; i < 10; i++){
+        // Numbers and buttons
+        sf::RectangleShape buttons[16];
+        sf::Text number[16];
+        for (int i = 0; i < 16; i++) {
+            float verti = (float)i/4;
+            float xaxis = 55 + (i % 4) * (26 + 53);
+            float yaxis = display.getPosition().y + 142 + verti * 35;
+            buttons[i].setSize(sf::Vector2f(53, 43));
+            buttons[i].setPosition(sf::Vector2f(xaxis, yaxis));
+            buttons[i].setFillColor(sf::Color::White);
+            buttons[i].setOutlineColor(sf::Color::Black);
+            buttons[i].setOutlineThickness(2);
+
             number[i].setFont(font);
             number[i].setCharacterSize(16);
             number[i].setFillColor(sf::Color::Black);
             number[i].setString(std::to_string(i));
-        }
-
-        int xaxis1, xaxis2, xaxis3;
-        xaxis1 = xaxis2 = xaxis3 = 100;
-        for(int i = 0; i < 10; i++){
-            //1st row
-            if(i > 6 && i <= 9){
-                number[i].setPosition(sf::Vector2f(xaxis1, display.getPosition().y + 130));
-                xaxis1 = xaxis1 + 100;
-            }
-            //2nd row
-            if(i > 3 && i < 7){
-                number[i].setPosition(sf::Vector2f(xaxis2, display.getPosition().y + 190));
-                xaxis2 = xaxis2 + 100;
-            }
-            //3rd row
-            if(i > 0 && i < 4){
-                number[i].setPosition(sf::Vector2f(xaxis3, display.getPosition().y + 250));
-                xaxis3 = xaxis3 + 100;
-            }
-            //4th row
-            if(i == 0){
-                number[i].setPosition(sf::Vector2f(200, display.getPosition().y + 310));
-            }
+            number[i].setPosition(buttons[i].getPosition() + sf::Vector2f(30, 10));
         }
         //display
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < 16; i++){
+            window.draw(buttons[i]);
             window.draw(number[i]);
         }
         window.display();
